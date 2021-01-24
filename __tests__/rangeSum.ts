15dfaee063d7c553e074f4bbb551d5ffb5a74b2b
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2021-01-24 23:51:41 +0900

    test update.

--- __tests__/rangeSum.ts
+++ __tests__/rangeSum.ts
@@ -2,6 +2,7 @@ import sum from "#/rangeSum";
 
 it("rangeSum Test", (): void => {
   expect(sum([1], [1], 0)).toBe(0);
+  expect(sum([1], [1], 1000)).toBe(1000);
   expect(sum([1, 11], [1, 2], 20)).toBe(30);
   expect(sum([1, 11, 21], [1, 2, 3], 30)).toBe(60);
   expect(sum([1, 11], [1, 2], 100)).toBe(190);
