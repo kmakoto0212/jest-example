e9864d53cccdeff276069b6a5d857119be96bcd7
kmakoto0212 <70053303+kmakoto0212@users.noreply.github.com>
2021-01-27 10:06:35 +0900

    refactor: 💡 Used Nullish coalescing operator

--- src/rangeSum.ts
+++ src/rangeSum.ts
@@ -1,7 +1,7 @@
 export default (range: number[], power: number[], num: number): number => {
   return range.reduce((prev, current, i, src): number => {
     if (current <= num) {
-      const next = src[i + 1] || 0;
+      const next = src[i + 1] ?? 0;
       const value = next
         ? Math.min(next - current, num - (current - 1))
         : num - (current - 1);
